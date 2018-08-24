523cec352bed58f7614151f2917573c85bae7e49
Ap-of-an <dmitry.virtyal@gmail.com>
2018-08-24 21:01:58 +0700

    refactoring

--- js/js.js
+++ js/js.js
@@ -1,101 +1,70 @@
-let canvas = document.getElementById("tutorial");
-let context = canvas.getContext("2d");
-canvas.setAttribute("width", canvas.parentElement.offsetWidth);
-canvas.setAttribute("height", canvas.parentElement.offsetHeight);
-const RAD_ = Math.PI / 180;
-const colors_spark = ["#135fbf", "#75a1ff", "#d6f0ff", "#2088bf"];
-context.lineWidth = 0.1;
-context.lineCap = 'round';
-context.lineJoin = 'round';
-let time = 0;
-const LT = 1000; //время жизни, за которое снежинка проходит путь от самого начала до конца
 class Spark {
   constructor() {
-    this.x = getRand(canvas.offsetWidth * 0.2, canvas.offsetWidth * 0.8);
+    this.x = getRand(canvas.width*0.1, canvas.width*0.8);
     this.y = 0;
-    this.y_0 = getRand(-1, -1);
-    this.rot_deg = getRand(-40, 40);
-    this.length = getRand(7, 11);
-    let a = getX1Y1(this.length, this.rot_deg, this.x, this.y);
-    this.deltaX = a.x1;
-    this.deltaY = a.y1;
-    this.x1 = () => {
-      return this.x + this.deltaX
-    };
-    this.y1 = () => {
-      return this.y + this.deltaY
-    };
-    this.color = getColor();
-    // начальная скорость неизвестна - конечная равняется 0
-    // vx vy -- начальные скорости
-    this.vx = 0;
-    this.vy = 2*(canvas.height/LT) - 0; // подразумевается vy пикселей в 1единицу времени ( 10 милисекунд)
-    this.ay = (0 - this.vy) / LT;
-    this.time = 0;
-    }
-  render() {
-    this.time += 1;
-    context.strokeStyle = this.color;
+    this.length = getRand(6, 15);
+    this.rotate_deg0 = getRand(-40, 40);
+    let temp = get_dx_dy(this.length, this.rotate_deg0, this.x, this.y);
+    this.x1 = () => { return this.x + temp.dx};
+    this.y1 = () => { return this.y + temp.dy};
+    this.v_angle = getRand(1,10) / 10;
+    this.vy = 2;
+    this.vx = 0;//() => { return this.vy*this.v_angle };
+    this.lw = 0.7;
+  }
+
+  func_moving() {}
+
+  create() {
+    context.beginPath();
+    context.lineWidth = this.lw;
     context.moveTo(this.x, this.y);
     context.lineTo(this.x1(), this.y1());
-    
     context.stroke();
-    //this.x += this.vx;
-    this.y = this.y_0 + this.vy * this.time + this.ay / 2 * this.time ** 2;
-  }
-}
-const N = 25;
-let arr_sparks = [];
-for (let i = 0; i < N; i++) {
-  arr_sparks.push(new Spark());
-}
-
-
-/* описание основных и вспомогательных функций */
-function load_canvas() {
-  if (canvas.getContext) {
-    /** аналог секундомера */
-    (function () {
-      let interval = 10;
-      let id = setInterval(function () {
-        time++;
-        draw_Canvas();
-      }, interval);
-      setInterval(function() {
-        time = 0;
-        arr_sparks.forEach(el => {
-          el.time = 0;
-        });
-      }, LT*10);
-    })()
-  } else {
-    console.log("Технология Canvas не поддерживается вашим браузером");
-    document.querySelector("body").style.backgroundColor = "red";
+    context.closePath();
+    this.func_moving();    
   }
 }
 
-setInterval(() => {
-  arr_sparks.push(new Spark());  
-}, 5000)
+class Sparks {
 
-function draw_Canvas() {
-  context.clearRect(0, 0, canvas.offsetWidth, canvas.offsetHeight);
-  context.beginPath();
-  for (const item of arr_sparks) {
-    item.render();
+  constructor(count_of_sparks) {
+    this.array = [];
+    for(let i =0; i < count_of_sparks; i++) {
+      this.array.push(new Spark());
+    }
   }
-  // window.requestAnimationFrame(draw_Canvas);
-}
+  
+  movingDown() {
+    this.array.forEach(el => {
+      el.func_moving = function() {
+        el.y += el.vy;
+      }
+    });
+  }
 
-function getX1Y1(length, anlge, x, y) {
-  let x1 = Math.abs(Math.sin(inRad(anlge))) * length;
-  let y1 = Math.cos(inRad(anlge)) * length;
+  render() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    this.movingDown();
+    this.array.forEach(el => {
+      el.create();
+      if(el.y > canvas.parentElement.offsetHeight*0.4 && el.lw > 0.05) {
+        el.lw -= 0.005;        
+      }
+    }); 
+    
+    // window.requestAnimationFrame(this.render); 
+  }
+}
+function get_dx_dy(length, anlge, x, y) {
+  let dx = Math.abs(Math.sin(inRad(anlge))) * length;
+  let dy = Math.cos(inRad(anlge)) * length;
   if (anlge < 0) {
-    x1 *= -1;
+    dx *= -1;
   }
   return {
-    'x1': x1,
-    'y1': y1
+    dx: dx,
+    dy: dy
   };
 }
 
@@ -106,14 +75,27 @@ function getColor() {
 function getRand(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
+const RAD_ = Math.PI / 180;
 function inRad(num) {
   return num * RAD_;
 }
 
-/* функции прослушки событий */
-window.addEventListener("resize", function () {
-  canvas.setAttribute("width", canvas.offsetParent.offsetWidth);
-  canvas.setAttribute("height", canvas.offsetParent.offsetHeight);
-  draw_Canvas();
-});
+let canvas = document.querySelector("canvas");
+let context = canvas.getContext("2d");
+canvas.setAttribute("width", canvas.parentElement.offsetWidth);
+canvas.setAttribute("height", canvas.parentElement.offsetHeight);
+context.lineCap = 'round';
+context.lineJoin = 'round';
+context.strokeStyle='#75a1ff';
+let bg_anim = new Sparks(1);
+document.body.onload = function() {
+  setInterval(()=> {
+    bg_anim.render();
+  }, 50);
+  setInterval(() => {
+    bg_anim.array.push(new Spark());
+    if(bg_anim.array.length > 66) {
+      bg_anim.array.shift();
+    }
+  }, 200)
+}
